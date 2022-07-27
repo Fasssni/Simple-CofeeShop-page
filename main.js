5a031acf105ab38e9ee6ded6306597bc075bb2c3
Insa Sadikov <sadikovinsaf99@gmail.com>
2022-07-28 00:56:18 +0300

    second commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    alert("Работает!")
+}
+console.log(10+10);
+let me="Insaf"
+console.log(`${me} is great programmer!`)
+let user={
+    name:'Insaf',
+    surname: 'Sadikov',
+    middlename:'Ilnurovich'
+
+};
+console.log(user.name)
